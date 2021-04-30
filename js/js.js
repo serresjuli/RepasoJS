051b86f5d28abb99c3ef597c29cc77dbf59f1e74
Juli <serresjuli@gmail.com>
2021-04-30 17:08:44 -0300

    VUELVO ATRAS CAMBIOS

---
+++ js/js.js
@@ -0,0 +1,21 @@
+
+let ingresarNumero = parseInt(prompt("ingrese numero"))
+
+for (let i = 1 ; i<= 10; i++) {
+    let resultado = ingresarNumero * i ;
+    alert( ingresarNumero + "X" + i + "=" + resultado)
+}
+
+// CLASE 2
+/*let Precio = prompt("ingrese precio")
+
+
+if (Precio < 20){
+    console.log("es menor a 20")
+}
+else if(Precio < 50){
+    console.log("es menor a 50")
+}
+else{
+    console.log( "es mayor a 50")
+}*/
